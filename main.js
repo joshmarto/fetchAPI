051ea08190efec35ead8e49740b4aaf21fc5a8dd
joshmarto <jmartinezolivares1@gmail.com>
2021-03-09 08:23:25 -0600

    Fetch mode setup

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ fetchData.addEventListener("click", setUp);
 
 function setUp() {
     newButton("data", "btn btn-info", "Refresh data");
-    fetch();
+    fetching();
 }
 
-function fetch() {
-    console.log("Fetching...");
+function fetching() {
     const APIkey = `AIzaSyBx7-DI3pkqzzfUQnvkjMz9uV4tzHO78JI`;
-    fetch(`https://maps.googleapis.com/maps/api/js?key=${APIkey}&callback=initMap`)
+    fetch(`https://maps.googleapis.com/maps/api/js?key=${APIkey}&callback=initMap`, { mode: 'no-cors' })
         .then(data => data.json())
         .then(d => displayInfo(d))
         .catch(err => console.log(err));
